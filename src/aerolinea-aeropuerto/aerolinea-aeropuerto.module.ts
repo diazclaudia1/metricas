715e19fb0f11cc543f2fa7681216c34b0492c353
claudia <cx.diaz@uniandes.edu.co>
2022-09-15 23:02:42 -0500

    logica

--- src/aerolinea-aeropuerto/aerolinea-aeropuerto.module.ts
+++ src/aerolinea-aeropuerto/aerolinea-aeropuerto.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AerolineaEntity } from 'src/aerolinea/aerolinea.entity';
+import { AeropuertoEntity } from 'src/aeropuerto/aeropuerto.entity';
 import { AerolineaAeropuertoService } from './aerolinea-aeropuerto.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AeropuertoEntity, AerolineaEntity])],
   providers: [AerolineaAeropuertoService]
 })
 export class AerolineaAeropuertoModule {}
